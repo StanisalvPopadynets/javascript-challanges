8a4424205760a034c4e61079c9e0c5395858bd44
Stanislav Popadynets <popadynetss@gmail.com>
2021-08-15 21:32:48 +0300

    add keyExtractor solution

---
+++ keyExtractor.js
@@ -0,0 +1,15 @@
+const extractKeys = (object) => {
+  const keys = []
+
+  const recursivelyFindKeys = (nestedObj) => {
+    for (key in nestedObj) {
+      if (typeof nestedObj[key] === "object") {
+        recursivelyFindKeys(nestedObj[key])
+      } else keys.push(key)
+    }
+  }
+  recursivelyFindKeys(object)
+  console.log(keys)
+}
+
+extractKeys({ a: 1, b: 1, c: { d: 1 } })
